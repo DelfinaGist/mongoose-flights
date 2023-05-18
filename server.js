25184551caa3fcab75711168759413250851fe13
DelfinaGist <fullstackcodez@gmail.com>
2023-05-18 17:45:53 -0500

    commit

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Listen
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
